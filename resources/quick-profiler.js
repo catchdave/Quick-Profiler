5c5bd502c14e25eb85314de947c8aeeb5eaf974a
David Rochwerger <drochwerger@trulia.com>
2012-06-09 21:05:04 -0700

    Dynamically load jquery if not available

--- resources/quick-profiler.js
+++ resources/quick-profiler.js
@@ -1,33 +1,73 @@
+// Dynamically load jQuery if not already loaded
+if (typeof jQuery === 'undefined') {
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js';
+	
+	// Onload
+	var QPLoaded = function() { $(document).ready(QP.init); };
+	if (script.readyState) {  //IE
+		script.onreadystatechange = function() {
+	        if (script.readyState === 'loaded' || script.readyState === 'complete') {
+	            script.onreadystatechange = null;
+	            QPLoaded();
+	        }
+	    };
+	} else {
+		script.onload = QPLoaded;
+	}
+	
+	document.body.appendChild(script);
+}
+
+/**
+ * Quick Profiler
+ */
 var QP = (function() {
-	var DETAILS_SHOWN = false,
-	    FULL_HEIGHT = false,
-	    container, // container reference
-	    pQp; // main DIV
+	var DETAILS_SHOWN = false,  // start with details shown?
+	    FULL_HEIGHT   = true, // whether to start with full height
+	    START_TINY    = false,  // start as the tiny panel?
+	    container; // container reference
 
 	var self = {
 
 		init : function() {
 			container = $('#pqp-container');
-			pQp = $('#pQp');
 
-			window.setTimeout(function() { container.css('display', 'block'); }, 10);
-			DETAILS_SHOWN = !container.hasClass('hideDetails');
+			self.toggleDetails(DETAILS_SHOWN);
+			if (DETAILS_SHOWN) {
+				self.toggleHeight(FULL_HEIGHT);
+			}
+			if (START_TINY) {
+				self.hide();
+			}
+			container.css('display', 'block');
 		},
 
+		/**
+		 * Hides/shows the metrics details
+		 */
 		toggleDetails : function(show) {
-			var show = (typeof show === 'undefined') ? DETAILS_SHOWN : show;
+			var show = (typeof show === 'undefined') ? !DETAILS_SHOWN : show;
 
 			if (show) {
-				container.addClass('hideDetails');
-				DETAILS_SHOWN = false;
-			} else {
 				container.removeClass('hideDetails');
 				DETAILS_SHOWN = true;
+			} else {
+				container.addClass('hideDetails');
+				DETAILS_SHOWN = false;
 			}
 		},
 
+		/**
+		 * Toggles height of details between normal and large
+		 */
 		toggleHeight : function(expand) {
 			var expand = (typeof expand === 'undefined') ? !FULL_HEIGHT : expand;
+			
+			if (!DETAILS_SHOWN) {
+				self.toggleDetails(true);
+			}
 			if (expand) {
 				container.addClass('tallDetails');
 				FULL_HEIGHT = true;
@@ -37,18 +77,20 @@ var QP = (function() {
 			}
 		},
 
+		/**
+		 * Change from one metric to another.
+		 */
 		changeTab : function(tab, selectedItem) {
-			$.each(pQp.find('.pqp-box, #pqp-metrics td'), function(i, el) {
-				if (el.id === '#pqp-' + tab) {
+			$.each(container.find('.pqp-box, #pqp-metrics td'), function(i, el) {
+				if (el.id === 'pqp-' + tab) {
 					$(el).addClass('selected');
 				} else {
 					$(el).removeClass('selected');
 				}
 			});
 			$(selectedItem).addClass('selected');
-			$('#pqp-' + tab).addClass('selected');
 
-			self.toggleDetails(false);
+			self.toggleDetails(true);
 		},
 
 		show : function() {
@@ -62,4 +104,6 @@ var QP = (function() {
 
 	return self;
 })();
-$(document).ready(QP.init);
+if (typeof jQuery !== 'undefined') {
+	$(document).ready(QP.init);
+}
